6a2bf344adc7a43228926f92c140bd6bbc058d2a
rafael <rafamendesmorais123@gmail.com>
2024-09-12 16:08:28 -0300

    add some getters and setters

--- src/domain/Entities/answer.ts
+++ src/domain/Entities/answer.ts
@@ -11,14 +11,49 @@ interface AnswerProps {
 }
 
 export class Answer extends Entity<AnswerProps>{
+
+  get questionId(){
+    return this.props.questionId
+  }
+
+  get authorId(){
+    return this.props.authorId
+  }
+
   get content(){
     return this.props.content
+  }
+  
+  get createdAt(){
+    return this.props.createdAt
+  }
+
+  get updatedAt(){
+    return this.props.updatedAt
+  }
+  
+  get execerpt() {
+    return this.content
+      .substring(0, 120)
+      .trimEnd()
+      .concat('...')
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  set content(content: string){
+    this.props.content = content
+    this.touch()
   }
 
   static create(
     props: Optional<AnswerProps, 'createdAt'>,
     id?: UniqueEntityId
-  ) {
+  )
+
+  {
     const answer = new Answer({
       ...props,
       createdAt: new Date(),
